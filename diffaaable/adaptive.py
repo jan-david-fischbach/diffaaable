5391270706742423993745692f95376c896b884f
Jan David Fischbach <mail@jan-dav.id>
2024-05-23 21:22:36 +0200

    add radius

--- diffaaable/adaptive.py
+++ diffaaable/adaptive.py
@@ -1,17 +1,21 @@
 import jax.numpy as np
 from diffaaable import aaa
 import jax
+from jax import random
 
-def _adaptive_aaa(z_k:np.ndarray,
+def _adaptive_aaa(z_k_0:np.ndarray,
                  f:callable,
-                 evolutions: int = 1,
+                 evolutions: int = 3,
                  cutoff: float = None,
-                 tol: float = 1e-13,
+                 tol: float = 1e-9,
+                 radius: float = 1e-3,
                  f_dot: callable = None):
   """
   z_k initial z_ks
   """
   collect_tangents = f_dot is not None
+  z_k = z_k_0
+  max_dist = np.max(np.abs(z_k_0[:, np.newaxis] - z_k_0[np.newaxis, :]))*0.4
 
   if collect_tangents:
     f_unpartial = f.func
@@ -33,22 +37,31 @@ def _adaptive_aaa(z_k:np.ndarray,
     m = np.abs(f_k)<cutoff #filter out values, that have diverged too strongly
     return z_k[m], f_k[m], f_k_dot[m]
 
+  key = random.key(0)
   for i in range(evolutions):
     z_k, f_k, f_k_dot = mask(z_k, f_k, f_k_dot)
     z_j, f_j, w_j, z_n = aaa(z_k, f_k, tol)
 
-    z_n_dot = np.zeros_like(z_n)
+    key, subkey = jax.random.split(key)
+
+    distance = np.min(np.abs(z_n[:, np.newaxis] - z_k_0[np.newaxis, :]), axis=-1)
+    dist_mask = distance < max_dist
+
+    add_z_k = z_n[dist_mask]
+    add_z_k += radius*np.exp(1j*2*np.pi*jax.random.uniform(subkey, add_z_k.shape))
+
+    add_z_k_dot = np.zeros_like(add_z_k)
     if collect_tangents:
       f_k_new , f_k_dot_new = jax.jvp(
-        f_unpartial, (*args, z_n), (*args_dot, z_n_dot)
+        f_unpartial, (*args, add_z_k), (*args_dot, add_z_k_dot)
       )
     else:
-      f_k_new = f(z_n)
+      f_k_new = f(add_z_k)
       f_k_dot_new = np.zeros_like(f_k_new)
 
     n_eval += len(z_n)
 
-    z_k = np.append(z_k, z_n)
+    z_k = np.append(z_k, add_z_k)
     f_k = np.append(f_k, f_k_new)
     f_k_dot = np.append(f_k_dot, f_k_dot_new)
 
@@ -56,6 +69,12 @@ def _adaptive_aaa(z_k:np.ndarray,
 
   if collect_tangents:
     return z_k, f_k, f_k_dot
+
+  import matplotlib.pyplot as plt
+  plt.scatter(z_k.real, z_k.imag)
+  plt.show()
+  plt.close()
+
   return z_j, f_j, w_j, z_n
 
 @jax.custom_jvp
