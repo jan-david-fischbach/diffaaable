3fb36892c88efb22ab6b4cb8eee5b18962a03f3a
Jan David Fischbach <mail@jan-dav.id>
2024-05-23 10:15:59 +0200

    custom JVP of nested function

--- tests/functional_gradients.py
+++ tests/functional_gradients.py
@@ -1,20 +1,45 @@
 # %%
 import jax
+import jax; jax.config.update('jax_platforms', 'cpu')
 import jax.numpy as np
 import jax.tree_util
 
-def f(x, a):
-  return x*a
+def f(a, b, x):
+  return x*a*b
 
-def nested(x, f_a):
+def nested_default(x, f_a):
   return f_a(x)
 
-def top_level(a):
-  f_a = jax.tree_util.Partial(f, a = a)
+def top_level(a, b):
+  f_a = jax.tree_util.Partial(f, a, b)
   x = 3.0
-  return nested(x, f_a)
+  return nested_default(x, f_a)
 
 # %%
 if __name__ == "__main__":
-  print(jax.grad(top_level)(2.0))
-# %%
+  print(jax.grad(top_level, argnums=(0,1))(2.0, 3.0))
+
+@jax.custom_jvp
+def nested(x, f_a):
+  return f_a(x)
+
+@nested.defjvp
+def nested_jvp(primals, tangents):
+  x, f_a = primals
+  f = f_a.func
+
+  a_flat, _ = jax.tree.flatten(f_a)
+
+  x_dot, f_a_dot = tangents
+  a_dot_flat, _ = jax.tree.flatten(f_a_dot)
+
+  primal_out, tangent_out = jax.jvp(f, (*a_flat, x), (*a_dot_flat, x_dot))
+  return primal_out, tangent_out
+
+def top_level(a, b):
+  f_a = jax.tree_util.Partial(f, a, b)
+  x = 3.0
+  return nested_default(x, f_a)
+
+if __name__ == "__main__":
+  print(jax.grad(top_level, argnums=(0,1))(2.0, 3.0))
