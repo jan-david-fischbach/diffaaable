5dfb08b7b40363d7d3c20eca48578b61e2ec2bca
Jan David Fischbach <mail@jan-dav.id>
2024-06-07 16:35:35 +0200

    working but inefficient selective aaa

--- diffaaable/selective.py
+++ diffaaable/selective.py
@@ -3,8 +3,23 @@ from diffaaable import aaa, residues
 from diffaaable.adaptive import Domain, domain_mask
 import matplotlib.pyplot as plt
 
+def reduced_domain(domain, reduction=1-0.125):
+  r = reduction
+  return (
+    domain[0]*r+domain[1]*(1-r),
+    domain[1]*r+domain[0]*(1-r)
+  )
+
+def sample_cross(domain):
+  center = domain_center(domain)
+  dist = 0.5 * (domain[1]-domain[0])
+  return center+np.array([dist.real, -dist.real, 1j*dist.imag, -1j*dist.imag])
+
 def sample_domain(domain: Domain, N: int):
+  if N==4:
+    return sample_cross(domain)
   sqrt_N = np.round(np.sqrt(N)).astype(int)
+  domain = reduced_domain(domain)
   z_k_r = np.linspace(domain[0].real, domain[1].real, sqrt_N)
   z_k_i = np.linspace(domain[0].imag, domain[1].imag, sqrt_N)
   Z_r, Z_i = np.meshgrid(z_k_r, z_k_i)
@@ -42,23 +57,31 @@ def plot_domain(domain: Domain):
 
   points = np.array([domain[0], right_down, domain[1], left_up, domain[0]])
 
-  return plt.plot(points.real, points.imag)
+  return plt.plot(points.real, points.imag, lw=np.abs(domain[1]-domain[0])/40)
 
 def all_poles_known(poles, prev, tol):
   if len(poles) == 0:
     return True
   if prev is None or len(prev)<len(poles):
     return False
+  # print("##########")
+  # print(f"poles: {poles}")
+  # print(f"prev: {prev}")
   dist = np.abs(poles[:, None] - prev[None, :])
-  return np.all(np.any(dist < tol, axis=-1))
+  print(np.min(dist, axis=1))
+  check = np.all(np.any(dist < tol, axis=1))
+  print(check)
+  return check
 
 def selective_refinement_aaa(f: callable,
                 domain: Domain,
+                z_k = np.array([], dtype=complex),
+                f_k = np.array([], dtype=complex),
                 N: int = 25,
-                max_poles: int = 3,
+                max_poles: int = 12,
                 cutoff: float = None,
                 tol_aaa: float = 1e-9,
-                tol_pol: float = 1e-4,
+                tol_pol: float = 1e-5,
                 suggestions = None):
   """
   """
@@ -67,27 +90,37 @@ def selective_refinement_aaa(f: callable,
   if cutoff is None:
     cutoff = np.inf
 
-  z_k = sample_domain(domain, N)
-  f_k = f(z_k) # should probably try to reuse...
+  z_k_new = sample_domain(domain, N)
+  f_k_new = f(z_k_new) # should probably try to reuse...
+
+  z_k = np.append(z_k, z_k_new)
+  f_k = np.append(f_k, f_k_new)
+  #plt.scatter(z_k.real, z_k.imag, color = plot_rect[0].get_color(), marker="+", alpha=0.2)
+
+
   z_j, f_j, w_j, z_n = aaa(z_k, f_k, tol=tol_aaa)
 
   poles = z_n[domain_mask(domain, z_n)]
+  plt.scatter(poles.real, poles.imag, color = "red", marker="+", alpha=0.2)
   if len(poles)<=max_poles and all_poles_known(poles, suggestions, tol_pol):
 
-    plt.scatter(poles.real, poles.imag, color = plot_rect[0].get_color())
+    plt.scatter(poles.real, poles.imag, color = plot_rect[0].get_color(), marker="x")
 
     res = residues(z_j, f_j, w_j, poles)
-    return poles, res
+    return poles, res, N
 
-  center = domain_center(domain)
-  subs = subdomains(domain, np.mean(np.array([np.mean(poles), center])))
+  weight = 0#0.3
+  pivot = weight*np.mean(poles) + (1-weight)*domain_center(domain)
+  subs = subdomains(domain, center=pivot)
 
   pol = np.empty((0,), dtype=complex)
   res = pol.copy()
+  evals = N
   for sub in subs:
-    p, r = selective_refinement_aaa(
-      f, sub, N, max_poles, cutoff, tol_aaa, tol_pol, suggestions=poles
+    p, r, e = selective_refinement_aaa(
+      f, sub, z_k_new, f_k_new, N, max_poles, cutoff, tol_aaa, tol_pol, suggestions=poles
     )
     pol = np.append(pol, p)
     res = np.append(res, r)
-  return pol, res
+    evals += e
+  return pol, res, evals
--- tests/test_selective.py
+++ tests/test_selective.py
@@ -10,12 +10,22 @@ def f(a, x):
     return np.tan(a*x)
 
 def test_fwd():
+    expected = np.arange(-40, 89)+0.5
     f_a = jax.tree_util.Partial(f, np.pi)
-    poles, residues = selective.selective_refinement_aaa(f_a, (-11-11j, 10+10j))
+    poles, residues, evals = selective.selective_refinement_aaa(f_a, (-41.1-3j, 90+10j))
     print(f"Poles: {poles}")
     print(f"Residues: {residues}")
+    print(f"Evaluations: {evals}")
+    print(f"Num Poles: {len(expected)}")
     plt.savefig("debug_out/selective.pdf")
     plt.close()
+
+    print("Hall of shame:")
+    for pole in expected:
+        hit = np.any(np.abs(poles-pole)< 1e-5)
+        if not hit:
+            print(pole)
+
 
 if __name__ == "__main__":
     test_fwd()
