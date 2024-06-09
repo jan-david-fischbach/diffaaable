d751cc0fd07489d432b705978bba1671aca4c965
Jan David Fischbach <mail@jan-dav.id>
2024-06-09 12:16:49 +0200

    7.5 samples per pole for 200 poles

--- diffaaable/selective.py
+++ diffaaable/selective.py
@@ -120,7 +120,7 @@ def selective_refinement_aaa(f: callable,
 
   eval_count = 0
   if use_adaptive:
-    folder = f"debug_out/{debug_name}"
+    folder = f"debug_out/{debug_name:o<12}"
     pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
     sampling = Partial(next_samples_heat, debug=folder,
                        stop=0.1, resolution=(101, 101), batchsize=10)
@@ -138,9 +138,10 @@ def selective_refinement_aaa(f: callable,
     eval_count -= len(z_k)
     z_j, f_j, w_j, z_n, z_k, f_k = adaptive_aaa(
       z_k, f, f_k_0=f_k, evolutions=N*16, tol=tol_aaa,
-      domain=reduced_domain(domain, 1.02), radius=4*domain_size/(N), #NOTE: actually increased domain :/
+      domain=reduced_domain(domain, 1.05), radius=4*domain_size/(N), #NOTE: actually increased domain :/
       return_samples=True, sampling=sampling, cutoff=np.inf
     )
+    # TODO pass down samples in buffer zone
     eval_count += len(z_k)
   else:
     if on_rim:
