d09fedb91539e36faabe621c173384abd8ff406f
Jan David Fischbach <mail@jan-dav.id>
2025-03-10 12:47:03 +0100

    norm power

--- diffaaable/tensor.py
+++ diffaaable/tensor.py
@@ -3,12 +3,12 @@ import numpy as np
 from baryrat import BarycentricRational
 from diffaaable.core import poles
 
-def tensor_aaa(z_k, F_k, thres_numerical_zero = 1e-13, aaa_tol=1e-9):
+def tensor_aaa(z_k, F_k, thres_numerical_zero = 1e-13, aaa_tol=1e-9, norm_power=0):
   total_vec = np.array([np.array(F_ki) for F_ki in F_k]).reshape(len(z_k), -1)
 
   norm = np.max(np.abs(total_vec), axis=0)
   numerical_zeros = norm < thres_numerical_zero
-  norm = norm**0.02
+  norm = norm**norm_power
 
   total_no_zeros = total_vec[:, ~numerical_zeros]
   norm_no_zeros = norm[~numerical_zeros]
@@ -35,13 +35,15 @@ def tensor_aaa(z_k, F_k, thres_numerical_zero = 1e-13, aaa_tol=1e-9):
   z_n = poles(z_j, w_j)
   return z_j, f_j, w_j, z_n
 
-def tensor_baryrat(z_j, f_j, w_j): #TODO write down properly
+def tensor_baryrat(z_j, f_j, w_j): #TODO write down properly (eg.g. using jax.vmap)
     shape = f_j[0].shape
     rs = [BarycentricRational(z_j, f_j_i, w_j) for f_j_i in f_j.reshape((len(z_j), -1)).T]
 
     def inner(z):
       z = np.array(z)
-      res = np.array([r(z) for r in rs])
-      return res.reshape((*z.shape, *shape))
+      results = [r(z) for r in rs]
+      res = np.stack(results, axis=-1)
+      out_shape = (*z.shape, *shape)
+      return res.reshape(out_shape)
 
     return inner
